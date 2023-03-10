8ff32e3ff6bd40fe63140ff2eeec8d98959c9693
Alobeast <alois.beard@gmail.com>
2023-03-10 14:24:14 +0100

    Another CRUD controller with Turbo Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
   Rails.application.routes.draw do
     devise_for :users
     resources :quotes
+  end
+
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
   end
 end
